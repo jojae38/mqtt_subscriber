2a2a7fcb53cb6dbd65cd660a3798fe5caef9050a
jojae38 <58541374+jojae38@users.noreply.github.com>
2022-05-11 10:40:59 +0900

    mqtt_send 기능 완성
    mqtt_send("명령", 채워넣을 array) 로 에뮬레이터에 값을 띄울 수 있음

--- include/mqtt_subscriber.h
+++ include/mqtt_subscriber.h
@@ -1,9 +1,9 @@
-
-
 #include <iostream>
 #include <ros/ros.h>
 #include <stdio.h>
 #include <mosquitto.h>
+#include <jsoncpp/json/json.h>
+#include <queue>
 
 #define ANSI_COLOR_RED "\x1b[31m" 
 #define ANSI_COLOR_GREEN "\x1b[32m" 
@@ -13,64 +13,7 @@
 #define ANSI_COLOR_CYAN "\x1b[36m" 
 #define ANSI_COLOR_RESET "\x1b[0m"
 
-const char *host="192.168.50.220";
-const int port = 1883;
-const std::string ID = "001";        
 
-const char* enum_ERROR[]={
-    "MOSQ_ERR_AUTH_CONTINUE",
-	"MOSQ_ERR_NO_SUBSCRIBERS",
-	"MOSQ_ERR_SUB_EXISTS",
-	"MOSQ_ERR_CONN_PENDING",
-	"MOSQ_ERR_SUCCESS",
-	"MOSQ_ERR_NOMEM",
-	"MOSQ_ERR_PROTOCOL",
-	"MOSQ_ERR_INVAL",
-	"MOSQ_ERR_NO_CONN",
-	"MOSQ_ERR_CONN_REFUSED",
-	"MOSQ_ERR_NOT_FOUND",
-	"MOSQ_ERR_CONN_LOST",
-	"MOSQ_ERR_TLS",
-	"MOSQ_ERR_PAYLOAD_SIZE",
-	"MOSQ_ERR_NOT_SUPPORTED",
-	"MOSQ_ERR_AUTH",
-	"MOSQ_ERR_ACL_DENIED",
-	"MOSQ_ERR_UNKNOWN",
-	"MOSQ_ERR_ERRNO",
-	"MOSQ_ERR_EAI",
-	"MOSQ_ERR_PROXY",
-	"MOSQ_ERR_PLUGIN_DEFER",
-	"MOSQ_ERR_MALFORMED_UTF8",
-	"MOSQ_ERR_KEEPALIVE",
-	"MOSQ_ERR_LOOKUP",
-	"MOSQ_ERR_MALFORMED_PACKET",
-	"MOSQ_ERR_DUPLICATE_PROPERTY",
-	"MOSQ_ERR_TLS_HANDSHAKE",
-	"MOSQ_ERR_QOS_NOT_SUPPORTED",
-	"MOSQ_ERR_OVERSIZE_PACKET",
-	"MOSQ_ERR_OCSP",
-};
-
-const char* enum_Order[]={
-    "AR",//자동운형
-    "AC",//운행 및 작업취소
-    "AP",//일시정지/해제
-    "AJ",//수동조작
-    "SL",//근접센서 설정
-    "SP",//네트워크 설정
-    "AW",//Action Work
-    "AG",//충전/충전해제
-    "AD",//지게차 모드
-    "TS",//T 맵 Set
-    "TG",//T 맵 Get
-    "GS",//G 맵 Set
-    "GG",//G 맵 받기
-    "GQ",//상태
-    "EI",//초기상태 (?)
-    "GB",//배터리 상태
-    "GD",//버전 상태
-    "DC" //수동 모드에서 작업삭제요청응답
-};
 /*SEND ORDER*/
 //AMRID 차량번호
 enum UpperType {none=0,Lift=1,CV_2=12,CV_3=13};
@@ -103,7 +46,6 @@ enum Manual_Complete {delete_order=1};
 
 /*RECEIVE ORDER*/
 
-
 void connect(struct mosquitto *mosq, void *obj, int rc);
 void message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg);
 void disconnect(struct mosquitto *mosq, void *obj, int rc);
@@ -111,14 +53,17 @@ void subscribe(struct mosquitto *mosq, void *obj,int mid,int qos_count,const int
 class mqtt_subscriber
 {
     public:
+        bool onetime;
         std::string topic_send;
         std::string topic_receive;
         void mqtt_connect(const char * host,const int port, const std::string ID);
+        void mqtt_send(const char* order,const void* array);
+        void mqtt_receive_request();
         void publish();
         mqtt_subscriber();
         ~mqtt_subscriber();
     private:
-
+        std::string convert_array_to_json(const char* order,const void* array);
         void Print_Error(int Error);
         int mqtt_init(const char * host,const int port, const std::string ID);
         struct mosquitto *mosq;
--- src/mqtt_subscriber.cpp
+++ src/mqtt_subscriber.cpp
@@ -1,23 +1,132 @@
 #include "mqtt_subscriber.h"
 
-// const char* enum_str[]={"AR","AC","AP","AD","DC"};
-mqtt_subscriber::mqtt_subscriber():topic_receive("ACS"+ID+">"+"AMR"+ID),topic_send(" AMR001>ACS001")
+const char *host="192.168.50.220";
+const int port = 1883;
+const std::string ID = "001";        
+
+const char* enum_ERROR[]={
+    "MOSQ_ERR_AUTH_CONTINUE",
+	"MOSQ_ERR_NO_SUBSCRIBERS",
+	"MOSQ_ERR_SUB_EXISTS",
+	"MOSQ_ERR_CONN_PENDING",
+	"MOSQ_ERR_SUCCESS",
+	"MOSQ_ERR_NOMEM",
+	"MOSQ_ERR_PROTOCOL",
+	"MOSQ_ERR_INVAL",
+	"MOSQ_ERR_NO_CONN",
+	"MOSQ_ERR_CONN_REFUSED",
+	"MOSQ_ERR_NOT_FOUND",
+	"MOSQ_ERR_CONN_LOST",
+	"MOSQ_ERR_TLS",
+	"MOSQ_ERR_PAYLOAD_SIZE",
+	"MOSQ_ERR_NOT_SUPPORTED",
+	"MOSQ_ERR_AUTH",
+	"MOSQ_ERR_ACL_DENIED",
+	"MOSQ_ERR_UNKNOWN",
+	"MOSQ_ERR_ERROR",
+	"MOSQ_ERR_EAI",
+	"MOSQ_ERR_PROXY",
+	"MOSQ_ERR_PLUGIN_DEFER",
+	"MOSQ_ERR_MALFORMED_UTF8",
+	"MOSQ_ERR_KEEPALIVE",
+	"MOSQ_ERR_LOOKUP",
+	"MOSQ_ERR_MALFORMED_PACKET",
+	"MOSQ_ERR_DUPLICATE_PROPERTY",
+	"MOSQ_ERR_TLS_HANDSHAKE",
+	"MOSQ_ERR_QOS_NOT_SUPPORTED",
+	"MOSQ_ERR_OVERSIZE_PACKET",
+	"MOSQ_ERR_OCSP"
+};
+
+const char* enum_Order[]={
+    "AR",//자동운형
+    "AC",//운행 및 작업취소
+    "AP",//일시정지/해제
+    "AJ",//수동조작
+    "SL",//근접센서 설정
+    "SP",//네트워크 설정
+    "AW",//Action Work
+    "AG",//충전/충전해제
+    "AD",//지게차 모드
+    "TS",//T 맵 Set
+    "TG",//T 맵 Get
+    "GS",//G 맵 Set
+    "GG",//G 맵 받기
+    "GQ",//상태
+    "EI",//초기상태 (?)
+    "GB",//배터리 상태
+    "GD",//버전 상태
+    "DC" //수동 모드에서 작업삭제요청응답
+};
+
+const char* EI[]={
+    "X",
+    "Y",
+    "H"
+};
+const char* GB[]={
+    "BCellVolt",
+    "BPackVolt",
+    "BPackCurrent",
+    "BChargeVolt",
+    "BTemperature",
+    "Battery",
+    "BError",
+    "BWarning",
+    "ErrorCode"
+};
+const char* GQ[]={
+    "X",
+    "Y",
+    "H",
+    "State",
+    "ActionID",
+    "Traffic",
+    "Mode",
+    "Clamp",
+    "Loaded",
+    "Level",
+    "CurrentNode",
+    "NextNode",
+    "TargetNode",
+    "Battery",
+    "Obstacle",
+    "Lift",
+    "Tilt",
+    "SideShift",
+    "ForkMover",
+    "PalletFrontX",
+    "PalletFrontY",
+    "PalletFrontTheta",
+    "PalletStackingX",
+    "PalletStackingY",
+    "PalletStackingTheta",
+    "TagId",
+    "ErrorCode"
+};
+const char* GD[]={
+    "TMapVersion",
+    "GMapVersion",
+    "FWVersion",
+    "ErrorCode"
+};
+mqtt_subscriber::mqtt_subscriber():topic_receive("ACS"+ID+">AMR"+ID),topic_send("AMR"+ID+">ACS"+ID),onetime(false)
 {
     // ros::NodeHandle nh("~");
     // std::string temp="AMR001";
 
     // nh.param("Robot_id",Robot_id,temp);
     mqtt_connect(host,port,ID);
-    getchar();
-    while(ros::ok())
-    {
-        //페어링 기능 완료시 다시 작동 x -> 연결 불가시 다시 페어링 모드로 복귀 -> 페어링 모드가 오래 지속될경우 베이스로 복귀
-        //연결 불가능 감지 및 행동
-        //센서 데이터 및 요청 데이터 publish
-        //일반 명령 체크 (queue) 명령이 0개일때는 mutex 작동 1개 이상일시는 mutex 작동 안함 
-        //긴급 명령 체크 예) 로봇 정지 및 종료
-        //로봇 제어
-    }
+    // getchar();
+    // while(ros::ok())
+    // {
+    //     //페어링 기능 완료시 다시 작동 x -> 연결 불가시 다시 페어링 모드로 복귀 -> 페어링 모드가 오래 지속될경우 베이스로 복귀
+    //     //연결 불가능 감지 및 행동
+    //     //센서 데이터 및 요청 데이터 publish
+    //     //일반 명령 체크 (queue) 명령이 0개일때는 mutex 작동 1개 이상일시는 mutex 작동 안함 
+    //     //긴급 명령 체크 예) 로봇 정지 및 종료
+    //     //로봇 제어
+    // }
 }
 mqtt_subscriber::~mqtt_subscriber()
 {
@@ -53,12 +162,14 @@ int mqtt_subscriber::mqtt_init(const char * host,const int port, const std::stri
     // mosquitto_disconnect_callback_set(mosq,disconnect);
     // mosquitto_subscribe_callback_set(mosq,subscribe);
     
-    int loop = mosquitto_loop_start(mosq);
+    // int loop = mosquitto_loop_start(mosq);
+    int loop = mosquitto_loop_forever(mosq,5,1);
     if(loop!=MOSQ_ERR_SUCCESS)
     {
         printf("mosquitto loop error\n");
         return loop;     
     }
+
     return MOSQ_ERR_SUCCESS;
 }
 void connect(struct mosquitto *mosq, void *obj, int rc)
@@ -79,7 +190,27 @@ void message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *
     // printf("New message with topic %s: %X\n",msg->topic,*(uint32_t*)msg->payload);
     char msg_con[msg->payloadlen];
     strcpy(msg_con,(const char*)msg->payload);
-    printf("%s, %s\n",msg->topic, msg_con);
+    printf("%s\n", msg_con);
+
+    Json::Value root;
+    Json::Reader reader;
+    reader.parse(msg_con,root);
+    if(root.get("Cmd","UTF-8")=="AR")
+    {
+        std::cout <<"aa"<<std::endl;
+    }
+    if(mos->onetime==false)
+    {
+        // int array[3]={10,10,10};
+        // int array[4]={5,2,20,20,};
+        int array[27]={1,1,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        // mos->mqtt_send_EI(array);
+        mos->mqtt_send("GQ",array);
+        mos->onetime=true;
+    }
+    
+    // std::cout <<root.get("Cmd","UTF-8")<<std::endl;
+    mosquitto_publish(mosq,NULL,(mos->topic_send).c_str(),msg->payloadlen,msg,0,0);
 
     //msg_payload 와 토픽에 따른 행동강령들
 }
@@ -95,10 +226,6 @@ void message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *
 //     printf("New disconnect %d, %d", *(int *)obj,rc);
 //     //연결 불가시 명령어 flag 온
 // }
-void mqtt_subscriber::publish()
-{
-    mosquitto_publish(mosq,NULL,topic_send.c_str(),sizeof(Status_Feedback),Status_Feedback,0,0);
-}
 void mqtt_subscriber::Print_Error(int Error)
 {
     Error+=4;
@@ -112,6 +239,60 @@ void mqtt_subscriber::Print_Error(int Error)
         printf(ANSI_COLOR_GREEN "OK" ANSI_COLOR_RESET "\n");
     }
 }
-//2개의 스레드가 필요
-//1 명령을 감지하는 놈 -> mosquitto_message_callback_set
-//2 명령을 처리하는 놈
+void mqtt_subscriber::mqtt_send(const char * order,const void* array)
+{
+    std::string msg=convert_array_to_json(order,array);
+    Print_Error(mosquitto_publish(mosq,NULL,topic_send.c_str(),msg.size(),msg.c_str(),0,0));
+}
+std::string mqtt_subscriber::convert_array_to_json(const char* order,const void* array)
+{   
+    Json::Value root;
+    Json::StyledWriter writer;
+    int arr_size;
+    int* temp_arr_int=(int*)array;
+    if(strcmp(order,"EI")==0)
+    {
+        // arr_size=sizeof(array)/sizeof(temp_arr_int[0]); 왜 array 가 8바이트이지?
+        arr_size=3;
+        root["Cmd"]="EI";
+        for(int i=0;i<arr_size;i++)
+        {
+            root[EI[i]]=(std::to_string(temp_arr_int[i]).c_str());
+        }
+    }        
+    else if(strcmp(order,"GB")==0)
+    {
+        // arr_size =sizeof(temp_arr_double)/sizeof(temp_arr_double[0]);
+        arr_size=9;
+        root["Cmd"]="GB";
+        root["Result"]="S";
+        for(int i=0;i<arr_size;i++)
+        {
+            root[GB[i]]=(std::to_string(temp_arr_int[i]).c_str());
+        }
+    } 
+    else if(strcmp(order,"GQ")==0)
+    {
+        // arr_size =sizeof(temp_arr_int)/sizeof(temp_arr_int[0]);
+        arr_size=27;
+        root["Cmd"]="GQ";
+        root["Result"]="S";
+        for(int i=0;i<arr_size;i++)
+        {
+            root[GQ[i]]=(std::to_string(temp_arr_int[i]).c_str());
+        }
+    }
+    else if(strcmp(order,"GD")==0)
+    {
+        // arr_size =sizeof(temp_arr_double)/sizeof(temp_arr_int[0]);
+        arr_size=4;
+        root["Cmd"]="GD";
+        root["Result"]="S";
+        for(int i=0;i<arr_size;i++)
+        {
+            root[GD[i]]=(std::to_string(temp_arr_int[i]).c_str());
+        }
+    }
+    std::string str=writer.write(root);
+    return str;
+}
